068587c465b8c1bc9524e9b4d47ab17bd5e55ba0
Oscar Catota <ocatotazurita@gmail.com>
2022-01-29 01:54:00 -0500

    pwa add module

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import vuetify from './plugins/vuetify'
 
 import axios from 'axios';
 import VueAxios from 'vue-axios';
+import './registerServiceWorker'
 
 Vue.config.productionTip = false
 Vue.use(VueAxios, axios);
